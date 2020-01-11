8cb446521210dfdcd14948e185011873353c6fef
Richard <fahitch+github@gmail.com>
2020-01-11 11:09:45 +0000

    add getName function to Passenger class. PassengerTest passes test

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -14,6 +14,8 @@ public class PassengerTest {
 
     @Test
     public void hasName(){
-        assertEquals("Tony", passnger.hasName());
+        assertEquals("Tony", passenger.getName());
     }
+
+
 }
