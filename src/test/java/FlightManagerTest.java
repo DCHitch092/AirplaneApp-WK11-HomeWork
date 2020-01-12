3a80b60569cbbf157432dcd99a67c2f929602294
Richard <fahitch+github@gmail.com>
2020-01-12 13:17:50 +0000

    FlightManager created

---
+++ src/test/java/FlightManagerTest.java
@@ -0,0 +1,8 @@
+public class FlightManagerTest {
+
+    //Calculate how much baggage weight should be reserved for each passenger for a flight
+    //calculate how much baggage weight is booked by passengers of a flight
+    //calculate how much overall weight reserved for baggage remains for a flight
+
+
+}
