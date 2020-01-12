244b8a021c3f5198f2b22245cfb0e4cc5d0439eb
Richard <fahitch+github@gmail.com>
2020-01-12 00:19:31 +0000

    add hasType to PlaneTest

--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlaneTest {
+
+    Passenger passenger;
+    Plane plane;
+
+    @Before
+    public void before(){
+        passenger = new Passenger("Tony", 2);
+        plane = new Plane(PlaneType.BOEING747);
+    }
+
+    @Test
+    public void hasType(){
+        assertEquals(PlaneType.BOEING747, plane.getPlaneType;);
+    }
 }
