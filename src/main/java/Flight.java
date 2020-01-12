0b69a4d8bb83f2ae6a4e4149dd619551d8f45035
Richard <fahitch+github@gmail.com>
2020-01-12 12:05:29 +0000

    FlightTest passes hasFlightNumber

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -3,10 +3,12 @@ import java.util.ArrayList;
 public class Flight {
     private ArrayList<Passenger> passengers;
     private Plane plane;
+    private String flightNumber;
 
-    public Flight(Plane plane){
+    public Flight(Plane plane, String flightNumber){
         this.passengers = new ArrayList<Passenger>();
         this.plane = plane;
+        this.flightNumber = flightNumber;
     }
 
     public ArrayList<Passenger> getPassengers() {
@@ -17,5 +19,7 @@ public class Flight {
         return plane;
     }
 
-
+    public String getFlightNumber() {
+        return flightNumber;
+    }
 }
